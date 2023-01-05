3c085314afe4c82f35f368bc18e4e173771b9c0e
8tg6w <xcmb@protonmail.com>
2023-01-05 14:11:19 +0100

    Create a basic skeleton of a Sphinx project.

---
+++ conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'PL Politics'
+copyright = '2023, 8tg6w'
+author = '8tg6w'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['docs', 'Thumbs.db', '.DS_Store', '.git', 'venv']
+
+language = 'pl'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_static_path = ['_static']
+html_title = 'PL Politics'
